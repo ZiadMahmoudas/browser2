d5ddf1a970ee8a7f2cb7efa811e6d3204f6343e6
Ziad Elmohager <ziadbobo78@gmail.com>
2025-08-19 12:43:59 +0300

    change2

---
+++ chunk-PXUWUTRQ.js
@@ -0,0 +1,1 @@
+import{Ja as n,cb as o,db as a,pb as c}from"./chunk-HMP5WNC5.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact"]],decls:2,vars:0,template:function(t,p){t&1&&(o(0,"p"),c(1,"contact works!"),a())},encapsulation:2})};export{s as Contact};
